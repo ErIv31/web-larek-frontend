33c391c98fa2d5a0029bf649d7238867de79359b
Ivan Ermolaev <eriv31@yandex.ru>
2025-03-30 18:41:47 +0300

    changing presenter folder

--- src/components/presenter/AppApi.ts
+++ src/components/presenter/AppApi.ts
@@ -1,35 +1,31 @@
 import { Api, ApiListResponse } from "../base/api";
-import { ICard, IOrder, IOrderResult } from "../../types/index";
+import { IProduct, IOrder, IOrderResult } from "../../types/index";
 
 export interface IAppApi {
-  getCardApi(id: string): Promise<ICard>;
-  getCardsArrayApi(): Promise<ICard[]>;
-  postOrderCardsApi(order: IOrder): Promise<IOrderResult>;
+  getProductList: () => Promise<IProduct[]>;
+  postOrder: (order: IOrder) => Promise<IOrderResult>;
 }
 
 export class AppApi extends Api implements IAppApi {
   readonly cdn: string;
+
   constructor(cdn: string, baseUrl: string, options?: RequestInit) {
     super(baseUrl, options);
     this.cdn = cdn;
   }
 
-  getCardApi(id: string): Promise<ICard> {
-      return this.get(`/product/${id}`).then((item: ICard) => ({
-        ...item, 
+  getProductList(): Promise<IProduct[]> {
+    return this.get(`/product`).then((data: ApiListResponse<IProduct>) => 
+      data.items.map((item) => ({
+        ...item,
         image: this.cdn + item.image
-      }));
+      }))
+    );
   }
 
-  getCardsArrayApi(): Promise<ICard[]> {
-      return this.get('/product').then((data: ApiListResponse<ICard>) => 
-        data.items.map((item) => ({
-          ...item, 
-          image: this.cdn + item.image
-        })));
-  }
-
-  postOrderCardsApi(order: IOrder): Promise<IOrderResult> {
-      return this.post('/order', order).then((data: IOrderResult) => data);
+  postOrder(order: IOrder): Promise<IOrderResult> {
+    return this.post('/order', order).then(
+      (data: IOrderResult) => data
+    );
   }
 }
