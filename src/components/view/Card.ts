8e15c52ffc3c31fa97a02f4855d35354f3f4da58
Ermolaev Ivan <eriv31@yandex.ru>
2025-04-01 18:04:51 +0300

    changing card

--- src/components/view/Card.ts
+++ src/components/view/Card.ts
@@ -22,6 +22,7 @@ export class Card<T> extends Component<ICard<T>> {
   protected _price: HTMLElement;
   protected _description?: HTMLElement;
   protected _button?: HTMLButtonElement;
+  protected _index?: HTMLElement;
 
 
   constructor(container: HTMLElement, actions?: ICardActions) {
@@ -31,8 +32,9 @@ export class Card<T> extends Component<ICard<T>> {
     this._title = ensureElement<HTMLElement>('.card__title', container);
     this._image = ensureElement<HTMLImageElement>('.card__image', container);
     this._price = ensureElement<HTMLElement>('.card__price', container);
-    this._description = container.querySelector('.card__description');
+    this._description = container.querySelector('.card__text');
     this._button = container.querySelector('.card__button');
+    this._index = container.querySelector('.basket__item-index');
 
     if (actions?.onClick) {
       if (this._button) {
@@ -77,121 +79,30 @@ export class Card<T> extends Component<ICard<T>> {
   }
 
   set price(value: number | null) {
-    this.setText(this._price, value ? `${value.toString()} синапсов` : `Бесценно`);
-  }
-}
-
-export class CardViewPreview extends CardView {
-  protected _category?: HTMLElement;
-  protected _description?: HTMLElement;
-  protected _image?: HTMLImageElement;
-  protected _button: HTMLButtonElement;
-  protected CardCategory: Record<string, string> = {
-    'софт-скил': '_soft',
-    'хард-скил': '_hard',
-    'другое': '_other',
-    'дополнительное': '_additional',
-    'кнопка': '_button'
-  };
-
-  constructor(container: HTMLElement, actions?: ICardActions) {
-    super(container);
-    this._category = container.querySelector('.card__category');
-    this._description = container.querySelector('.card__text');
-    this._image = container.querySelector('.card__image');
-    this._button = container.querySelector('.button');
-
-    if (actions?.onClick) {
-      if (this._button) {
-        this._button.addEventListener('click', actions.onClick);
-      } else {
-        container.addEventListener('click', actions.onClick);
-      }
-    }
+    if (value !== null) {
+      this.setText(this._price, `${value} синапсов`);
+    } else {
+      this.setText(this._price, 'Бесценно');
+    }    
   }
 
-  set category(value: string) {
-    this._category.textContent = value;
-    this._category.className = '';
-    this._category.classList.add(this._category.classList[0]);
-    this._category.classList.add(`this._category.classList[0]${this.CardCategory[value]}`);
-  }
-
-  set description(value: string) {
+  set description(value: string | string[]) {
     this.setText(this._description, value);
   }
 
-  set image(value: string) {
-    this._image.src = value;
-    this._image.alt = this._title.textContent;
+  set index(value: string) {
+    this.container.dataset.id = value;
   }
 
-  set button(value: string) {
-    this.setText(this._button, value);
+  get index(): string {
+    return this.container.dataset.id || '';
   }
 
-  disableButton() {
-    this.setDisabled(this._button, true);
-  }
-}
-
-export class CardViewArray extends CardView {
-  protected _category?: HTMLElement;
-  protected _image?: HTMLImageElement;
-  protected CardCategory: Record<string, string> = {
-    'софт-скил': '_soft',
-    'хард-скил': '_hard',
-    'другое': '_other',
-    'дополнительное': '_additional',
-    'кнопка': '_button'
-  };
-
-  constructor(container: HTMLElement, actions?: ICardActions) {
-    super(container);
-    this._category = container.querySelector('.card__category');
-    this._image = container.querySelector('.card__image');
-
-    if (actions?.onClick) {
-      if (this._image) {
-        this._image.addEventListener('click', actions.onClick);
-      } else {
-        container.addEventListener('click', actions.onClick);
-      }
+  toggleButton(value: boolean) {
+    if (value) {
+      this.setText(this._button, 'Удалить'); 
+    } else {
+      this.setText(this._button, 'В корзину');
     }
-  }
-
-  set category(value: string) {
-    this._category.textContent = value;
-    this._category.className = '';
-    this._category.classList.add(this._category.classList[0]);
-    this._category.classList.add(`this._category.classList[0]${this.CardCategory[value]}`);
-  }  
-
-  set image(value: string) {
-    this._image.src = value;
-    this._image.alt = this._title.textContent;
-  }
-}
-
-export class CardViewInBasket extends CardView {
-  protected button?: HTMLButtonElement;
-  protected _index: HTMLElement;
-
-  constructor(container: HTMLElement, actions?: ICardActions) {
-    super(container);
-    this.button = container.querySelector('.basket__item-delete');
-    this._index = container.querySelector('.basket__item-index');
-
-    if (actions?.onClick) {
-      if (this.button) {
-        this.button.addEventListener('click', actions.onClick);
-      } else {
-        container.addEventListener('click', actions.onClick);
-      }
-    }
-  }
-
-  set index(value: number) {
-    this._index.textContent = value.toString();
   }
 }
